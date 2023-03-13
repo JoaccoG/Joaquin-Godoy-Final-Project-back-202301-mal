9927d33da9d2fd2724c5a889791a970ef5c4b0c2
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-13 19:09:14 +0100

    Add initial server config

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import log from './logger.js';
+import app from './app.js';
+import { connectDB } from './database/mongodb.js';
+
+dotenv.config();
+
+const port = process.env.PORT ?? 3000;
+const mongoUrl = process.env.MONGO_URL ?? '';
+
+app.listen(port, async () => {
+  await connectDB(mongoUrl);
+  log.info(`Server started in port ${port}`);
+});
