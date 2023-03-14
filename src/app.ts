35c26bebf0239eb283906dd73ceb8733cd317987
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-14 02:21:15 +0100

    Remove unused authMiddleware from app

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import authRouter from './api/auth/auth-router.js';
 import apiRouter from './api/api-router.js';
-import { authMiddleware } from './api/auth/auth-middleware.js';
 import { errorHandler } from './utils/error-handlers.js';
 
 const app = express();
@@ -16,7 +15,7 @@ app.use(express.json());
 app.disable('x-powered-by');
 
 app.use('/auth', authRouter);
-app.use('/api/v1', authMiddleware, apiRouter);
+app.use('/api/v1', apiRouter);
 
 app.use(errorHandler);
 
