2e217b991c7577ed3b663341b31cd8c59508a136
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-14 02:17:39 +0100

    Add main errors handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import authRouter from './api/auth/auth-router.js';
+import apiRouter from './api/api-router.js';
+import { authMiddleware } from './api/auth/auth-middleware.js';
+import { errorHandler } from './utils/error-handlers.js';
 
 const app = express();
 
@@ -10,5 +14,10 @@ app.get('/', (_req, res) => {
 app.use(cors());
 app.use(express.json());
 app.disable('x-powered-by');
+
+app.use('/auth', authRouter);
+app.use('/api/v1', authMiddleware, apiRouter);
+
+app.use(errorHandler);
 
 export default app;
