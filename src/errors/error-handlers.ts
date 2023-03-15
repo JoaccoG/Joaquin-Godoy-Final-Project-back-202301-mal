eb414d21ff9a1b50103a939c801a8d762c564a5b
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-15 19:50:39 +0100

    Modify error handlers to add custom http error

--- src/errors/error-handlers.ts
+++ src/errors/error-handlers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { ValidationError } from 'express-validation';
 import log from '../logger.js';
+import { CustomHTTPError } from './custom-http-error.js';
 
 export const errorHandler = (
   err: Error,
@@ -9,10 +10,16 @@ export const errorHandler = (
   _next: NextFunction,
 ) => {
   if (err instanceof ValidationError) {
+    return res
+      .status(err.statusCode)
+      .json({ msg: err.details.body?.[0].message ?? err.message });
+  }
+
+  if (err instanceof CustomHTTPError) {
     log.error(err);
-    return res.status(err.statusCode).json(err);
+    return res.status(err.httpCode).json(err.toBodyJSON());
   }
 
   log.error(err);
-  return res.status(500).json(err);
+  return res.status(500).json({ msg: err.message });
 };
