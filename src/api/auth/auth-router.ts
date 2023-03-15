b4b35f13de66a7e94ff5ee3f92a49885cf39a008
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-15 20:18:05 +0100

    Add joi validation to auth router

--- src/api/auth/auth-router.ts
+++ src/api/auth/auth-router.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import { validate } from 'express-validation';
-import { errorHandler } from '../../errors/error-handlers.js';
 import { loginController, registerController } from './auth-controllers.js';
 import { authValidation } from './auth-validations.js';
 
 const authRouter = express.Router();
 
-authRouter.use(validate(authValidation));
+authRouter.use(validate(authValidation, {}, {}));
 
-authRouter.route('/register').post(errorHandler, registerController);
-authRouter.route('/login').post(errorHandler, loginController);
+authRouter.route('/register').post(registerController);
+authRouter.route('/login').post(loginController);
 
 export default authRouter;
