eeb6841bdb8bd95187ac51f67def8f21236c4f2b
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-17 19:09:55 +0100

    Add body parser to main app file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import authRouter from './api/auth/auth-router.js';
 import apiRouter from './api/api-router.js';
 import { appErrorHandler } from './errors/error-handlers.js';
 import { authMiddleware } from './api/auth/auth-middleware.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.get('/', (_req, res) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 app.disable('x-powered-by');
 
 app.use('/auth', authRouter);
