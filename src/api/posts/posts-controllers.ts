eb4a0fb7f996742ff065c1665091af1d2b704d9a
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-17 19:47:57 +0100

    Add create post controller

---
+++ src/api/posts/posts-controllers.ts
@@ -0,0 +1,74 @@
+import { RequestHandler } from 'express';
+import { PROFILE_BUCKET_NAME, supabase } from '../../database/supabase.js';
+import { CustomHTTPError } from '../../errors/custom-http-error.js';
+import log from '../../logger.js';
+import { GameModel } from '../games/games-schema.js';
+import { UserModel } from '../users/users-schema.js';
+import { Post, PostModel } from './posts-schema.js';
+
+export const createNewPostController: RequestHandler<
+  { gameId: string },
+  Post,
+  Pick<Post, 'review' | 'rating' | 'photo'>,
+  unknown,
+  { id: string }
+> = async (req, res, next) => {
+  const user = res.locals.id;
+  const { review, rating } = req.body;
+  const { gameId } = req.params;
+  const fileBuffer = req.file?.buffer;
+  const fileName = `PostPhoto-${user}-${Date.now()}.webp`;
+
+  try {
+    let newPost = {
+      user,
+      game: gameId,
+      review,
+      rating,
+      photo: '',
+      likes: 0,
+      date: Date.now(),
+    };
+
+    if (fileBuffer !== undefined) {
+      const { error } = await supabase.storage
+        .from(PROFILE_BUCKET_NAME)
+        .upload(fileName, fileBuffer, {
+          upsert: true,
+        });
+
+      if (error === null) {
+        const { data } = await supabase.storage
+          .from(PROFILE_BUCKET_NAME)
+          .getPublicUrl(fileName);
+
+        newPost = {
+          ...newPost,
+          photo: data.publicUrl,
+        };
+      }
+    }
+
+    const post = await PostModel.create(newPost);
+    const userRes = await UserModel.updateOne(
+      { _id: user },
+      { $push: { posts: post._id } },
+    );
+    if (userRes.matchedCount === 0) {
+      throw new CustomHTTPError(404, 'User not found');
+    }
+
+    const gameRes = await GameModel.updateOne(
+      { _id: gameId },
+      { $push: { posts: post._id } },
+    );
+    if (gameRes.matchedCount === 0) {
+      throw new CustomHTTPError(404, 'Game not found');
+    }
+
+    log.info('New post successfully created');
+    return res.status(201).json(post);
+  } catch (err) {
+    next(err);
+  }
+};
