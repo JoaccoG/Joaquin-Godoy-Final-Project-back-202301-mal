7f19fc4678237349d4df0866a8222d5905a6471b
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-18 02:23:18 +0100

    Add get method to posts router

--- src/api/posts/posts-router.ts
+++ src/api/posts/posts-router.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createNewPostController } from './posts-controllers.js';
+import {
+  createNewPostController,
+  getAllPostsController,
+} from './posts-controllers.js';
 import uploadGameImg from './posts-img-middleware.js';
 
 export const postsRouter = express.Router();
 
+postsRouter.route('/').get(getAllPostsController);
 postsRouter
   .route('/:gameId')
   .post(uploadGameImg.single('photo'), createNewPostController);
