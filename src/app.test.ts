ede07a53db543e7d5abada1d34e90137acf56e7c
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-21 19:18:00 +0100

    Add token validation tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,9 +1,26 @@
 import request from 'supertest';
+import { generateJWTToken } from './api/auth/auth-utils';
 import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const token = generateJWTToken('userId');
 
 describe('Given an app', () => {
   test('When the server starts, then the route app path should have a Server ON message', async () => {
     const res = await request(app).get('/');
     expect(res.body).toEqual('Server ON');
+  });
+
+  test('When the user wants to validate his token with a valid token, then the response should be a 200', async () => {
+    const res = await request(app)
+      .get('/token-validation')
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(200);
+  });
+
+  test('When the user wants to validate his token with an invalid token, then the response should be a 401', async () => {
+    const res = await request(app).get('/token-validation');
+    expect(res.status).toBe(401);
   });
 });
