802cdbaeee875452e9aaa59ee9220a203e0d878b
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-24 12:04:00 +0100

    Change mocked posts file (utils) according to new tests cases

---
+++ src/api/posts/tests/utils.ts
@@ -0,0 +1,20 @@
+export const mockedPosts = [
+  {
+    _id: 1,
+    user: 'user_id1',
+    game: 'game_id1',
+    review: 'review',
+    rating: 1,
+    photo: 'https://example.com/photo.png',
+    date: 123456789,
+  },
+  {
+    _id: 2,
+    user: 'user_id2',
+    game: 'game_id2',
+    review: 'review',
+    rating: 3,
+    photo: '',
+    date: 123456789,
+  },
+];
