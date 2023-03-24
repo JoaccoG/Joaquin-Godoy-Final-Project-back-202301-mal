3c0ce1c0e63b5e2bdc8d44c743639dbfa0361100
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-24 13:16:21 +0100

    Add joi validation to post method of posts entity router

--- src/api/posts/posts-router.ts
+++ src/api/posts/posts-router.ts
@@ -1,16 +1,22 @@
 import express from 'express';
+import { validate } from 'express-validation';
 import upload from '../../database/multer.js';
 import {
   createNewPostController,
   deletePostController,
   getAllPostsController,
 } from './posts-controllers.js';
+import { postValidation } from './posts-validation.js';
 
 export const postsRouter = express.Router();
 
 postsRouter
   .route('/')
   .get(getAllPostsController)
-  .post(upload.single('photo'), createNewPostController);
+  .post(
+    upload.single('photo'),
+    validate(postValidation, {}, {}),
+    createNewPostController,
+  );
 
 postsRouter.route('/:idPost').delete(deletePostController);
