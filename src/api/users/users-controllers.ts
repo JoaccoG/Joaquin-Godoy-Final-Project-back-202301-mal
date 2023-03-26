db940df913f145faea6bb716df1ec80e34de023c
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-26 05:14:44 +0200

    Add get user posts by id controller

--- src/api/users/users-controllers.ts
+++ src/api/users/users-controllers.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from 'express';
 import { CustomHTTPError } from '../../errors/custom-http-error.js';
+import { PostModel } from '../posts/posts-schema.js';
 import { UserModel } from './users-schema.js';
 
 export const getUserByIdController: RequestHandler = async (req, res, next) => {
@@ -31,6 +32,37 @@ export const getUserByIdController: RequestHandler = async (req, res, next) => {
       userFollowersCount: userData.followers.length,
       userFollowingCount: userData.following.length,
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUserPostsByIdController: RequestHandler<
+  { idUser: string },
+  unknown,
+  unknown,
+  {
+    offset: number;
+    limit: number;
+  }
+> = async (req, res, next) => {
+  const { idUser } = req.params;
+  const { offset, limit } = req.query;
+
+  try {
+    const postsCount = await PostModel.countDocuments({ user: idUser }).exec();
+
+    const posts = await PostModel.find({ user: idUser })
+      .sort({ date: -1 })
+      .limit(limit)
+      .skip(offset)
+      .populate({ path: 'user', select: 'username name surname avatar' })
+      .populate({ path: 'game', select: 'name banner' })
+      .exec();
+
+    return res
+      .status(200)
+      .json({ msg: 'Successfully fetched posts!', count: postsCount, posts });
   } catch (err) {
     next(err);
   }
