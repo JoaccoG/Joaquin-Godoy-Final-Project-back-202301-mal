5a4873beb55b409df69f4bb480aff6ee80f5883a
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-26 05:12:26 +0200

    Add users entity endpoint to api router

--- src/api/api-router.ts
+++ src/api/api-router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { postsRouter } from './posts/posts-router.js';
+import { usersRouter } from './users/users-router.js';
 
 const apiRouter = express.Router();
 
 apiRouter.use('/posts', postsRouter);
+apiRouter.use('/users', usersRouter);
 
 export default apiRouter;
