0b61129714b53e3124b68bd9bc741b4720758b1a
JoaccoG <joaquingodoy2407@gmail.com>
2023-04-23 02:47:53 +0200

    Add selection and conditional statement to games get method

--- src/api/games/games-controllers.ts
+++ src/api/games/games-controllers.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
-import { GameModel } from './games-schema';
-import { RequestQueryOffsetLimit } from '../../types/models';
+import { GameModel } from './games-schema.js';
+import { RequestQueryOffsetLimit } from '../../types/models.js';
+import { CustomHTTPError } from '../../errors/custom-http-error.js';
 
 export const getGamesController: RequestHandler<
   unknown,
@@ -11,12 +12,20 @@ export const getGamesController: RequestHandler<
   const { offset, limit } = req.query;
 
   try {
+    if (!limit || limit > 10) {
+      throw new CustomHTTPError(
+        400,
+        'Query parameter "limit" is missing or is greater than 10',
+      );
+    }
+
     const gamesCount = await GameModel.countDocuments().exec();
 
     const games = await GameModel.find({})
       .sort({ launch: -1 })
       .limit(limit)
       .skip(offset)
+      .select('_id name banner')
       .exec();
 
     return res.status(200).json({
